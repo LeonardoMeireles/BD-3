c0e63c902cf4fb317877b166cbddce52f363d9a5
LeonardoMeireles <leo.meireles@usp.br>
2021-12-08 20:01:18 -0300

    Treating HTML and CSS, added background pic

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import homeRoutes from './routers/homeRouters'
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 80
 
 // Sets EJS as view engine
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(homeRoutes)
 
-app.listen(port, () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`))
+app.listen(port, () => console.log(`⚡️[server]: Server is running at http://localhost:${port}`))
