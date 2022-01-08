6aafda7da24b015141e2e1e762864cf36684a077
Lucas Vilela <lk-vila@protonmail>
2022-01-08 17:08:59 -0300

    Connects app to postgres db

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import homeRoutes from './routers/homeRouters'
 import path from 'path'
+const { Client } = require('pg')
 
 const app = express()
-const port = process.env.PORT || 80
+const PORT = process.env.PORT || 8000;
+const PASS = process.env.POSTGRES_PASSWORD
 
 // Sets EJS as view engine
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,4 +14,24 @@ app.set('views', 'views');
 
 app.use(homeRoutes)
 
-app.listen(port, () => console.log(`⚡️[server]: Server is running at http://localhost:${port}`))
+
+async function main(){
+
+    const client = new Client({
+        user: 'postgres',
+        host: '127.0.0.1',
+        database: 'postgres',
+        password: `${PASS}`,
+        port: 5432,
+      })
+    await client.connect()
+
+    app.listen(PORT, () =>
+        console.log(
+            `⚡️ [server]: Server is running at https://localhost:${PORT}`
+        )
+    );
+
+}
+
+main()
